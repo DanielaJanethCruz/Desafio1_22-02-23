f5d82e2d40292fd2ac05a781c08c2863eb94a710
DanielaJanethCruz <daniela122cruz.11@gmail.com>
2023-02-24 01:00:40 -0600

    First Commit Desafios

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
-import { Product, products } from "../products";
+import { Component, Input } from "@angular/core";
+import { Product } from "../products";
+import { DialogModule } from "primeng/dialog";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product",
@@ -7,14 +9,26 @@ import { Product, products } from "../products";
   styleUrls: ["./product.component.css"],
 })
 export class ProductComponent {
-  products = [...products];
+  display: boolean = false;
+
+  constructor(private cartService: CartService) {}
 
   @Input() product!: Product;
 
   share() {
     window.alert("The product has been shared!");
   }
+
   onNotify() {
     window.alert("You will be notified when the product goes on sale");
+  }
+
+  showDialog() {
+    this.display = true;
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
   }
 }
--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  images: string[];
 }
 
 export const products = [
@@ -11,35 +12,41 @@ export const products = [
     name: "Phone XL",
     price: 799,
     description: "A large phone with one of the best screens",
+    images: ["Phonexl.jpg"],
   },
   {
     id: 2,
     name: "Phone Mini",
     price: 699,
     description: "A great phone with one of the best cameras",
+    images: ["PhoneMini.jpg"],
   },
   {
     id: 3,
     name: "Phone Standard",
     price: 299,
     description: "A normal phone with normal specs",
+    images: ["PhoneStandar.jpg"],
   },
   {
     id: 4,
     name: "Tablet XL",
     price: 799,
     description: "A large tablet with one of the best screens",
+    images: ["Tabletxl.jpg"],
   },
   {
     id: 5,
     name: "Tablet Mini",
     price: 699,
     description: "A great tablet with one of the best cameras",
+    images: ["TabletMini.jpg"],
   },
   {
     id: 6,
     name: "Tablet Standard",
     price: 299,
     description: "A normal tablet with normal specs",
+    images: ["TabletStandar.jpg"],
   },
 ];
